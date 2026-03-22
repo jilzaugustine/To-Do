80b5481f2f7bb9200591c538fda27f528825d2f5
jilzaugustine <jilzaugustine04@gmail.com>
2026-03-22 22:30:14 +0530

    completed tasks added plus new namespacing for adding task function

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -23,5 +23,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    path('todo/', include(('todo.urls', 'todo'), namespace='todo'))
+    path('todo/', include(('todo.urls', 'todo'), namespace='todo')) 
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -7,7 +7,10 @@ from todo.models import task
 def home(request):
     tasks=task.objects.filter(is_completed=False).order_by('-updated_at')
     print(tasks)
+    completed_tasks=task.objects.filter(is_completed=True)
+    print(completed_tasks)
     context={
         'tasks':tasks,
+        'completed_tasks':completed_tasks
     }
     return render(request,'home.html',context)
